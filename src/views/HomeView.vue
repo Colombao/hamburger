4fc002844620f5248229f77ff387bbbda7ed47f9
João Vitor <joao.vitor@customax.local>
2023-05-30 09:53:34 -0300

    pronto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Banner />
+  <div class="main-container">
+   <h1>Monte o seu burguer</h1>
+   <BurgerForm />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Banner from '../components/banner.vue';
+import BurgerForm from '../components/burguerform.vue';
 
 export default {
-  name: 'HomeView',
+  name: 'Home',
   components: {
-    HelloWorld
+    Banner,
+    BurgerForm
+    }, 
   }
-}
+
+
 </script>
---
+++ src/views/Pedidos.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="main-container">  
+  <Dashboard />
+</div>
+</template>
+<script>
+import Dashboard from '../components/dashboard.vue';
+export default{
+  name: 'Pedidos',
+  components: {
+    Dashboard
+  }
+}
+</script>
