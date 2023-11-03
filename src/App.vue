b47d4d986246817a9a05098eef5aec76e2d39db1
Joao <joaoocolombo@gmail.com>
2023-11-03 20:35:41 -0300

    Login_Teste

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,15 @@
 <template>
-  <Navbar :logo="logo_src" :alt="app_name" />
-  <router-view/>
-  <Footer />
+    <div>
+        <ModernScreen />
+    </div>
 </template>
+
 <script>
-import Footer from './components/footer.vue';
-import Navbar from './components/navbar.vue';
-
-export default{
-  components:{
-    Navbar,
-    Footer
-  },
-  data(){
-    return{
-      logo_src: "/img/logo.png",
-      app_name: "Make Your Burguer"
-    }
-  }
-}
-
+import ModernScreen from "./components/login.vue"; // Importe o componente ModernScreen.vue
+export default {
+    components: {
+        ModernScreen, // Registre o componente para usá-lo no seu template
+    },
+    // Restante do código da sua outra tela
+};
 </script>
-<style>
-*{
-  font-family: Arial, Helvetica, sans-serif;
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-}
-.main-container{
-  margin: 50px;
-  min-height: 300px;
-}
-h1{
-  text-align: center;
-  font-size: 42px;
-  margin-bottom: 30px;
-  color: #222;
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -12,9 +12,6 @@ const routes = [{
   {
     path: '/Pedidos',
     name: 'Pedidos',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import( /* webpackChunkName: "Pedidos" */ '../views/Pedidos.vue')
   }
 ]
