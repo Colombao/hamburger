342a814eda8a90bb3e0ffc21d82c001e1e721820
Colombao <joaoocolombo@gmail.com>
2025-06-24 21:38:29 -0300

    Atualiza informações de burgers e funcionários no banco de dados; ajusta estilo do componente Navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,43 +1,43 @@
 <template>
-    <div id="nav">
-        <router-link to="/" id="logo-url">
-            <img :src="logo" :alt="alt" id="logo" />
-        </router-link>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/Pedidos">Pedidos</router-link> |
-        <router-link to="/Sobre">Sobre Nós</router-link> |
-        <router-link to="/Funcionario">Funcionario</router-link>
-    </div>
+  <div id="nav">
+    <router-link to="/" id="logo-url">
+      <img :src="logo" :alt="alt" id="logo" />
+    </router-link>
+    <router-link to="/Home">Home</router-link> |
+    <router-link to="/Pedidos">Pedidos</router-link> |
+    <router-link to="/Sobre">Sobre Nós</router-link> |
+    <router-link to="/Funcionario">Funcionario</router-link>
+  </div>
 </template>
 <script>
 export default {
-    name: "Navbar",
-    props: ["logo", "alt"],
+  name: "Navbar",
+  props: ["logo", "alt"],
 };
 </script>
 <style scoped>
 #nav {
-    background-color: #222;
-    border-bottom: 4px solid #111;
-    padding: 15px 50px;
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
+  background-color: #222;
+  border-bottom: 4px solid #111;
+  padding: 15px 50px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
 }
 #nav #logo-url {
-    margin: auto;
-    margin-left: 0;
+  margin: auto;
+  margin-left: 0;
 }
 #logo {
-    width: 40px;
+  width: 40px;
 }
 #nav a {
-    color: #fcba03;
-    text-decoration: none;
-    margin: 12px;
-    transition: 0.5s;
+  color: #fcba03;
+  text-decoration: none;
+  margin: 12px;
+  transition: 0.5s;
 }
 #nav a:hover {
-    color: #fff;
+  color: #fff;
 }
 </style>
